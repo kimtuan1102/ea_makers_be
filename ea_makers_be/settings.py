49bc7949a9a655baa1832b325b7b32fcf77dceed
trinh <kimtuan1102@gmail.com>
2020-07-29 09:52:25 +0700

    fix django filter query term

--- ea_makers_be/settings.py
+++ ea_makers_be/settings.py
@@ -130,10 +130,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework_simplejwt.authentication.JWTAuthentication',
-    ),
-    'DEFAULT_FILTER_BACKENDS': (
-        'rest_framework_filters.backends.RestFrameworkFilterBackend'
-    ),
+    )
     # 'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     # 'PAGE_SIZE': 100,
 }
