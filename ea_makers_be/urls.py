68725cdd2b99f05f0e0fb112ff1b4d71a28640d1
kimtuan1102 <kimtuan1102@gmail.com>
2020-08-05 11:45:03 +0700

    add zalo notify

--- ea_makers_be/urls.py
+++ ea_makers_be/urls.py
@@ -38,7 +38,7 @@ schema_view = get_schema_view(
 )
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('api/admin/', admin.site.urls),
     path('api/token/', CustomTokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
